380199849570e47657bb9e861de6d0ae34a7f497
miris-mp <miriam.petrocchi94@gmail.com>
2023-07-21 16:33:07 +0200

    Added fano component and route to access it

---
+++ src/Website/src/app/about/fano/fano.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fano',
+  templateUrl: './fano.component.html',
+  styleUrls: ['./fano.component.css']
+})
+export class FanoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
