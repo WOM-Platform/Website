4496faa2606346fb50250c0bfc071b24ad52c2a3
miris-mp <miriam.petrocchi94@gmail.com>
2023-08-02 18:16:26 +0200

    Added tot graph and translation date picker

--- src/Website/src/app/user/stats/merchant/data.ts
+++ src/Website/src/app/user/stats/merchant/data.ts
@@ -2,28 +2,34 @@ export var single = [
     {
         "name": "10% caffè",
         "value": 40632,
+        "feedback": 3
     },
     {
         "name": "Paghi 1 prendi 2",
         "value": 50000,
+        "feedback": 3
     },
     {
         "name": "Cornetto con caffè omaggio",
         "value": 36745,
+        "feedback": 3
     },
     {
         "name": "15% dolci",
         "value": 36240,
+        "feedback": 3
     },
     {
         "name": "15% salati",
         "value": 33000,
+        "feedback": 4
     },
     {
         "name": "Aperitivo 3x4",
         "value": 35800,
         "extra": {
             "code": "it"
-        }
+        },
+        "feedback": 3
     }
 ];
--- src/Website/src/app/user/stats/merchant/merchant-stats.component.ts
+++ src/Website/src/app/user/stats/merchant/merchant-stats.component.ts
@@ -1,20 +1,39 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { single } from './data';
-import {Merchant, MerchantContainer} from "../../../_models";
+import { MerchantContainer } from "src/app/_models";
+
 
 @Component({
     selector: 'app-merchant-stats',
     templateUrl: './merchant-stats.component.html',
     styleUrls: ['./merchant-stats.component.css']
 })
-export class MerchantStatsComponent {
-    @Input()
-    merchants: MerchantContainer[];
+export class MerchantStatsComponent implements OnInit{
+    @Input() merchants: MerchantContainer[];
+
+    totComparison  : {name: string, value: number}[] = [{name: "total", value: 0}, {name: "average", value: 160000}]
+
+    viewAverage = [700, 200]
+    tot = 0
+    averageValue = 160000
+    units: string = 'WOM bruciati';
 
     single: any[];
-    view: any[] = undefined;
+    view: [number, number] = [1000, 400];
+    viewTotComparison : [number, number] = [400, 500]
+    autoScale: true;
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  
+  
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'WOM consumati';
 
     // options
     tooltipDisabled: boolean = false;
@@ -24,12 +43,26 @@ export class MerchantStatsComponent {
     showLabels: boolean = true;
     isDoughnut: boolean = false;
 
-    colorScheme = {
-        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    colorsTotComparison = {
+        domain: ['#464555','#FF566F']
+    }
+    colorsWOMConsumati = {
+        domain: ['#2A69FF', '#DD52DB', '#FF57A3', '#FF8A70', '#FFC457', '#F9F871']
     };
 
     constructor() {
         Object.assign(this, { single });
+    }
+
+    
+    ngOnInit(): void {
+        // Sum all the offerts to know how many WOM where consume
+        for(let i = 0; i < this.single.length; i++) {[0]
+            console.log(this.single[i].value)
+            this.totComparison[0].value = +this.single[i].value + this.totComparison[0].value
+            this.tot = +this.single[i].value + this.tot
+        }
+        console.log(this.totComparison)
     }
 
     onSelect(data): void {
