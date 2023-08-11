63762d71844c8cc3e3d0b354cce052b1ef8959da
miris-mp <miriam.petrocchi94@gmail.com>
2023-08-11 16:40:33 +0200

    Remove duplicate

--- src/Website/src/app/app.module.ts
+++ src/Website/src/app/app.module.ts
@@ -186,7 +186,6 @@ export const isMock = environment.mock;
         ReactiveFormsModule,
         SharedModule,
         TranslateModule,
-        TranslateModule,
         LayoutModule,
     ],
     providers: [
