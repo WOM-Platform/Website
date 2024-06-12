1bf24f84db69cea12d8e51ba7611264074077d55
miris-mp <miriam.petrocchi94@gmail.com>
2024-06-12 19:03:46 +0200

    User table setup

--- src/Website/src/app/_services/user.service.ts
+++ src/Website/src/app/_services/user.service.ts
@@ -1,283 +1,326 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { BehaviorSubject, Observable } from "rxjs";
-import { map } from "rxjs/operators";
-import { User, UserLogin, UserRegistrationPayload } from "../_models";
-import { environment } from "../../environments/environment";
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {BehaviorSubject, Observable, of, throwError} from "rxjs";
+import {catchError, map, tap} from "rxjs/operators";
+import {User, UserLogin, UserRegistrationPayload} from "../_models";
+import {environment} from "../../environments/environment";
+import {StorageService} from "./storage.service";
 
-@Injectable({ providedIn: "root" })
+@Injectable({providedIn: "root"})
 export class UserService {
-  localUrlV1 = environment.baseUrl + environment.v1 + "user/";
-  localUrlV2 = environment.baseUrl + environment.v2 + "user/";
+    localUrlV1 = environment.baseUrl + environment.v1 + "user/";
+    localUrlV2 = environment.baseUrl + environment.v2 + "user/";
 
-  // Login data
-  public currentUserLogin: Observable<UserLogin>;
-  private currentUserLoginSubject: BehaviorSubject<UserLogin>;
+    // Login data
+    public currentUserLogin: Observable<UserLogin>;
+    private currentUserLoginSubject: BehaviorSubject<UserLogin>;
 
-  // User data
-  public currentUser: Observable<User>;
-  private currentUserSubject: BehaviorSubject<User>;
+    // User data
+    public currentUser: Observable<User>;
+    private currentUserSubject: BehaviorSubject<User>;
 
-  private userOwnership: BehaviorSubject<any> = new BehaviorSubject<any>({});
+    private userOwnership: BehaviorSubject<any> = new BehaviorSubject<any>({});
 
-  public userOwnershipStatus = this.userOwnership.asObservable();
+    public userOwnershipStatus = this.userOwnership.asObservable();
 
-  constructor(private http: HttpClient) {
-    const localStorageUserLogin = UserLogin.fromJson(
-      JSON.parse(localStorage.getItem("currentUserLogin"))
-    );
-    this.currentUserLoginSubject = new BehaviorSubject<UserLogin>(
-      localStorageUserLogin
-    );
-    this.currentUserLogin = this.currentUserLoginSubject.asObservable();
+    constructor(private http: HttpClient, private storageService: StorageService) {
+        const localStorageUserLogin = UserLogin.fromJson(
+            JSON.parse(localStorage.getItem("currentUserLogin"))
+        );
+        this.currentUserLoginSubject = new BehaviorSubject<UserLogin>(
+            localStorageUserLogin
+        );
+        this.currentUserLogin = this.currentUserLoginSubject.asObservable();
 
-    const localStorageUser = User.fromJson(
-      JSON.parse(localStorage.getItem("currentUser"))
-    );
-    this.currentUserSubject = new BehaviorSubject<User>(localStorageUser);
-    this.currentUser = this.currentUserSubject.asObservable();
-  }
+        const localStorageUser = User.fromJson(
+            JSON.parse(localStorage.getItem("currentUser"))
+        );
+        this.currentUserSubject = new BehaviorSubject<User>(localStorageUser);
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
 
-  public get currentUserValue(): User {
-    return this.currentUserSubject.value;
-  }
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
 
-  public get currentUserLoginValue(): UserLogin {
-    return this.currentUserLoginSubject.value;
-  }
+    public get currentUserLoginValue(): UserLogin {
+        return this.currentUserLoginSubject.value;
+    }
 
-  async checkAuthenticated(): Promise<boolean> {
-    return (
-      this.currentUserSubject.getValue() !== null &&
-      this.currentUserSubject.getValue() !== undefined
-    );
-  }
+    async checkAuthenticated(): Promise<boolean> {
+        return (
+            this.currentUserSubject.getValue() !== null &&
+            this.currentUserSubject.getValue() !== undefined
+        );
+    }
 
-  /**
-   * Login and retrieve the user id and bearer token
-   * @param email username is always email
-   * @param password password associated for the username
-   */
-  login(email: string, password: string): Observable<any> {
-    return this.http
-      .post<any>(
-        this.localUrlV1 + "login",
-        {
-          /*
-            email,
-            password,
-            clientName: '',
-            persistent: true
-            */
-        },
-        {
-          observe: "response",
-          headers: {
-            Authorization: "Basic " + btoa(email + ":" + password),
-          },
+    /**
+     * Login and retrieve the user id and bearer token
+     * @param email username is always email
+     * @param password password associated for the username
+     */
+    login(email: string, password: string): Observable<any> {
+        return this.http
+            .post<any>(
+                this.localUrlV1 + "login",
+                {
+                    /*
+                      email,
+                      password,
+                      clientName: '',
+                      persistent: true
+                      */
+                },
+                {
+                    observe: "response",
+                    headers: {
+                        Authorization: "Basic " + btoa(email + ":" + password),
+                    },
+                }
+            )
+            .pipe(
+                map((response) => {
+                    // login successful if there's a valid user in the response
+                    if (response.body) {
+                        const login = UserLogin.fromJson(response.body);
+                        // store user details and jwt token in local storage to keep user logged in between page refreshes
+                        localStorage.setItem("currentUserLogin", JSON.stringify(login));
+                        this.currentUserLoginSubject.next(login);
+                        return login;
+                    }
+                })
+            );
+    }
+
+    /**
+     * Notify the server that the user has logged out & remove local storage data
+     */
+    logout(): void {
+        this.http.post<any>(this.localUrlV1 + "logout", {}).pipe(
+            map((value) => {
+                console.log("logout: " + value);
+            })
+        );
+
+        // remove user from local storage to log user out
+        localStorage.removeItem("currentUserLogin");
+        localStorage.removeItem("currentUser");
+        localStorage.removeItem("merchantData");
+        localStorage.removeItem("instrumentData");
+
+        this.currentUserLoginSubject.next(null);
+        this.currentUserSubject.next(null);
+    }
+
+    /**
+     * Get user data for current user id
+     */
+    getLoggedUser(): Observable<User> {
+        return this.http
+            .get<User>(this.localUrlV1 + this.currentUserLoginSubject.value.id)
+            .pipe(
+                map((user) => {
+                    // user id is correct if value is not null
+                    if (user) {
+                        // hydrate a full User object from its JSON representation (to have its methods/logic)
+                        user = User.fromJson(user);
+
+                        // store user details in local storage to save user data in between page refreshes
+                        localStorage.setItem("currentUser", JSON.stringify(user));
+                        this.currentUserSubject.next(user);
+                    }
+                    return user;
+                })
+            );
+    }
+
+    /**
+     * Register a new user
+     * @param data user data
+     */
+    register(data: UserRegistrationPayload): Observable<User> {
+        return this.http
+            .post<User>(this.localUrlV1 + "register", data)
+            .pipe(map((response) => response));
+    }
+
+    /**
+     * Update an existing user's data
+     * @param data user data
+     */
+    update(data: UserRegistrationPayload): Observable<User> {
+        return this.http
+            .post<User>(this.localUrlV1 + this.currentUserLoginSubject.value.id, data)
+            .pipe(map((response) => response));
+    }
+
+    /**
+     * verify status of user account
+     */
+    verify(userId: string = null): Observable<any> {
+        const id = userId != null ? userId : this.currentUserLoginSubject.value.id;
+        return this.http
+            .post<any>(this.localUrlV1 + id + "/verify", {})
+            .pipe(map((response) => response));
+    }
+
+    /**
+     * verify status of user account
+     */
+    sendVerification(userId: string, token: string): Observable<any> {
+        return this.http
+            .post<any>(this.localUrlV1 + userId + "/verify?token=" + token, {})
+            .pipe(map((response) => response));
+    }
+
+    /**
+     * request new verification email using user id
+     */
+    requestVerificationEmailById(userId: string = null): Observable<any> {
+        const id = userId != null ? userId : this.currentUserLoginSubject.value.id;
+        return this.http
+            .post<any>(this.localUrlV1 + id + "/request-verification", {})
+            .pipe(map((response) => response));
+    }
+
+    /**
+     * request new verification using e-mail
+     */
+    requestVerificationEmailByEmail(email: string = null): Observable<any> {
+        const e = email != null ? email : this.currentUserValue.email;
+        return this.http
+            .post<any>(this.localUrlV1 + "request-verification", {
+                email: e,
+            })
+            .pipe(map((response) => response));
+    }
+
+    /**
+     * ask for password reset
+     * @param email registered user's email (username)
+     */
+    passwordResetRequest(email: string): Observable<any> {
+        return this.http
+            .post<any>(this.localUrlV1 + "password-reset", {email})
+            .pipe(map((response) => response));
+    }
+
+    /**
+     * Send new password
+     * @param userId user's id to reset password for
+     * @param token confirmation token
+     * @param password new password
+     */
+    passwordReset(
+        userId: string,
+        token: string,
+        password: string
+    ): Observable<any> {
+        return this.http
+            .post<any>(this.localUrlV1 + userId + "/password-reset", {
+                token,
+                password,
+            })
+            .pipe(map((response) => response));
+    }
+
+    me(): Observable<any> {
+        return this.http.get<any>(this.localUrlV1 + "me").pipe(
+            map((user) => {
+                user = User.fromJson(user);
+
+                // store user details in local storage to save user data in between page refreshes
+                localStorage.setItem("currentUser", JSON.stringify(user));
+                this.currentUserSubject.next(user);
+
+                localStorage.setItem("merchantData", JSON.stringify(user.merchants));
+                localStorage.setItem("instrumentData", JSON.stringify(user.sources));
+                return user;
+            })
+        );
+    }
+
+    updateUserOwnership(data: any) {
+        this.userOwnership.next(data);
+    }
+
+    userCreate(
+        name: string,
+        surname: string,
+        email: string,
+        password: string
+    ): Observable<any> {
+        const body = {
+            name: name,
+            surname: surname,
+            email: email,
+            password: password,
+            verified: true,
+            role: "User",
+        };
+
+        return this.http.post<any>(this.localUrlV1, body).pipe(
+            map((res) => {
+                return res;
+            })
+        );
+    }
+
+    userSearch(name: string = "", email: string = ""): Observable<any> {
+        let page = 1;
+        let pageSize = 10;
+        const params: HttpParams = new HttpParams()
+            .set("name", name.toString())
+            .set("email", email.toString())
+            .set("page", page.toString())
+            .set("pageSize", pageSize.toString());
+
+        return this.http.get<any>(this.localUrlV1, {params}).pipe(
+            map((res) => {
+                return res;
+            })
+        );
+    }
+
+    getAllUsers(search: string,
+                page: number,
+                itemsPerPage: string = "10"): Observable<any> {
+        const cachedUsers = this.storageService.get("usersList");
+        if (cachedUsers) {
+            return of(cachedUsers);
+        } else {
+            const params = new HttpParams()
+                .set("search", search)
+                .set("page", page.toString())
+                .set("pageSize", itemsPerPage);
+            return this.http.get(`${this.localUrlV1}`, {params}).pipe(
+                tap({
+                    next: (data) => this.storageService.set("usersList", data),
+                    error: (err) => console.error("err ", err),
+                }),
+                map((res) => {
+                    return res;
+                }),
+                catchError((err) => {
+                    console.error("Error fetching instruments", err);
+                    return throwError(() => new Error("Failed to fetch instruments"));
+                })
+            );
         }
-      )
-      .pipe(
-        map((response) => {
-          // login successful if there's a valid user in the response
-          if (response.body) {
-            const login = UserLogin.fromJson(response.body);
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem("currentUserLogin", JSON.stringify(login));
-            this.currentUserLoginSubject.next(login);
-            return login;
-          }
-        })
-      );
-  }
+    }
 
-  /**
-   * Notify the server that the user has logged out & remove local storage data
-   */
-  logout(): void {
-    this.http.post<any>(this.localUrlV1 + "logout", {}).pipe(
-      map((value) => {
-        console.log("logout: " + value);
-      })
-    );
+// Delete user
+    delete(idUser: string): Observable<any> {
+        return this.http.delete(`${this.localUrlV1}${idUser}`).pipe(
+            tap({
+                next: () => console.log(`Deleted user with id=${idUser}`),
+                error: (err) => console.error(`Failed to delete user with id=${idUser}`, err)
+            }),
+            catchError(err => {
+                console.error('Delete operation failed', err);
+                return throwError(() => new Error('Failed to delete user'));
+            })
+        );
+    }
 
-    // remove user from local storage to log user out
-    localStorage.removeItem("currentUserLogin");
-    localStorage.removeItem("currentUser");
-    localStorage.removeItem("merchantData");
-    localStorage.removeItem("instrumentData");
-
-    this.currentUserLoginSubject.next(null);
-    this.currentUserSubject.next(null);
-  }
-
-  /**
-   * Get user data for current user id
-   */
-  getLoggedUser(): Observable<User> {
-    return this.http
-      .get<User>(this.localUrlV1 + this.currentUserLoginSubject.value.id)
-      .pipe(
-        map((user) => {
-          // user id is correct if value is not null
-          if (user) {
-            // hydrate a full User object from its JSON representation (to have its methods/logic)
-            user = User.fromJson(user);
-
-            // store user details in local storage to save user data in between page refreshes
-            localStorage.setItem("currentUser", JSON.stringify(user));
-            this.currentUserSubject.next(user);
-          }
-          return user;
-        })
-      );
-  }
-
-  /**
-   * Register a new user
-   * @param data user data
-   */
-  register(data: UserRegistrationPayload): Observable<User> {
-    return this.http
-      .post<User>(this.localUrlV1 + "register", data)
-      .pipe(map((response) => response));
-  }
-
-  /**
-   * Update an existing user's data
-   * @param data user data
-   */
-  update(data: UserRegistrationPayload): Observable<User> {
-    return this.http
-      .post<User>(this.localUrlV1 + this.currentUserLoginSubject.value.id, data)
-      .pipe(map((response) => response));
-  }
-
-  /**
-   * verify status of user account
-   */
-  verify(userId: string = null): Observable<any> {
-    const id = userId != null ? userId : this.currentUserLoginSubject.value.id;
-    return this.http
-      .post<any>(this.localUrlV1 + id + "/verify", {})
-      .pipe(map((response) => response));
-  }
-
-  /**
-   * verify status of user account
-   */
-  sendVerification(userId: string, token: string): Observable<any> {
-    return this.http
-      .post<any>(this.localUrlV1 + userId + "/verify?token=" + token, {})
-      .pipe(map((response) => response));
-  }
-
-  /**
-   * request new verification email using user id
-   */
-  requestVerificationEmailById(userId: string = null): Observable<any> {
-    const id = userId != null ? userId : this.currentUserLoginSubject.value.id;
-    return this.http
-      .post<any>(this.localUrlV1 + id + "/request-verification", {})
-      .pipe(map((response) => response));
-  }
-
-  /**
-   * request new verification using e-mail
-   */
-  requestVerificationEmailByEmail(email: string = null): Observable<any> {
-    const e = email != null ? email : this.currentUserValue.email;
-    return this.http
-      .post<any>(this.localUrlV1 + "request-verification", {
-        email: e,
-      })
-      .pipe(map((response) => response));
-  }
-
-  /**
-   * ask for password reset
-   * @param email registered user's email (username)
-   */
-  passwordResetRequest(email: string): Observable<any> {
-    return this.http
-      .post<any>(this.localUrlV1 + "password-reset", { email })
-      .pipe(map((response) => response));
-  }
-
-  /**
-   * Send new password
-   * @param userId user's id to reset password for
-   * @param token confirmation token
-   * @param password new password
-   */
-  passwordReset(
-    userId: string,
-    token: string,
-    password: string
-  ): Observable<any> {
-    return this.http
-      .post<any>(this.localUrlV1 + userId + "/password-reset", {
-        token,
-        password,
-      })
-      .pipe(map((response) => response));
-  }
-
-  me(): Observable<any> {
-    return this.http.get<any>(this.localUrlV1 + "me").pipe(
-      map((user) => {
-        user = User.fromJson(user);
-
-        // store user details in local storage to save user data in between page refreshes
-        localStorage.setItem("currentUser", JSON.stringify(user));
-        this.currentUserSubject.next(user);
-
-        localStorage.setItem("merchantData", JSON.stringify(user.merchants));
-        localStorage.setItem("instrumentData", JSON.stringify(user.sources));
-        return user;
-      })
-    );
-  }
-
-  updateUserOwnership(data: any) {
-    this.userOwnership.next(data);
-  }
-
-  userCreate(
-    name: string,
-    surname: string,
-    email: string,
-    password: string
-  ): Observable<any> {
-    const body = {
-      name: name,
-      surname: surname,
-      email: email,
-      password: password,
-      verified: true,
-      role: "User",
-    };
-
-    return this.http.post<any>(this.localUrlV1, body).pipe(
-      map((res) => {
-        return res;
-      })
-    );
-  }
-
-  userSearch(name: string = "", email: string = ""): Observable<any> {
-    let page = 1;
-    let pageSize = 10;
-    const params: HttpParams = new HttpParams()
-      .set("name", name.toString())
-      .set("email", email.toString())
-      .set("page", page.toString())
-      .set("pageSize", pageSize.toString());
-
-    return this.http.get<any>(this.localUrlV1, { params }).pipe(
-      map((res) => {
-        return res;
-      })
-    );
-  }
 }
--- src/Website/src/app/user/aims/user-aims.component.ts
+++ src/Website/src/app/user/aims/user-aims.component.ts
@@ -1,53 +1,49 @@
 import {
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  OnInit,
-  Output,
+    ChangeDetectorRef,
+    Component,
+    OnInit,
 } from "@angular/core";
-import { AdminTableComponent } from "../components/admin-table/admin-table.component";
-import { AimsService } from "../../_services";
-import { Aim, Titles } from "../../_models";
-import { NgIf } from "@angular/common";
-import { LoadingService } from "../../_services/loading.service";
-import { StorageService } from "../../_services/storage.service";
+import {AimsService} from "../../_services";
+import {Aim} from "../../_models";
+
 
 @Component({
-  selector: "app-aims-tab",
-  templateUrl: "./user-aims.component.html",
-  styleUrl: "./user-aims.component.css",
+    selector: "app-aims-tab",
+    templateUrl: "./user-aims.component.html",
+    styleUrl: "./user-aims.component.css",
 })
 export class UserAimsComponent implements OnInit {
-  aimsTableColumns: any[] = [
-    { field: "code", hideOnMobile: false },
-    { field: "it", hideOnMobile: false },
-    { field: "en", hideOnMobile: false },
-    { field: "hidden", hideOnMobile: false },
-  ];
+    aimsTableColumns: any[] = [
+        {field: "code", hideOnMobile: false},
+        {field: "it", hideOnMobile: false},
+        {field: "en", hideOnMobile: false},
+        {field: "hidden", hideOnMobile: false},
+    ];
 
-  aimsData: any[];
+    aimsData: any[];
 
-  constructor(
-    private aimsService: AimsService,
-    private cdr: ChangeDetectorRef
-  ) {}
+    constructor(
+        private aimsService: AimsService,
+        private cdr: ChangeDetectorRef
+    ) {
+    }
 
-  ngOnInit() {
-    this.aimsService.getAll().subscribe({
-      next: (res) => {
-        this.aimsData = res.map((aim: Aim) => {
-          let transformedAim = {
-            code: aim.code,
-            it: aim.titles.it,
-            en: aim.titles.en,
-            hidden: aim.hidden,
-          };
-          return transformedAim;
+    ngOnInit() {
+        this.aimsService.getAll().subscribe({
+            next: (res) => {
+                this.aimsData = res.map((aim: Aim) => {
+                    let transformedAim = {
+                        code: aim.code,
+                        it: aim.titles.it,
+                        en: aim.titles.en,
+                        hidden: aim.hidden,
+                    };
+                    return transformedAim;
+                });
+
+                this.cdr.detectChanges();
+            },
+            error: (err) => console.error(err),
         });
-
-        this.cdr.detectChanges();
-      },
-      error: (err) => console.error(err),
-    });
-  }
+    }
 }
