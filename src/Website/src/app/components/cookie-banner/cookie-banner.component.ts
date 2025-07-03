cdfcec0972d7460d16f0847412908f8195b1baba
Your Name <miriam.petrocchi94@gmail.com>
2025-07-03 17:02:55 +0200

    Fix: display cookie banner when user visits

--- src/Website/src/app/components/cookie-banner/cookie-banner.component.ts
+++ src/Website/src/app/components/cookie-banner/cookie-banner.component.ts
@@ -1,78 +1,74 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { BehaviorSubject, Subject, takeUntil } from "rxjs";
 import { CookieConsentService } from "src/app/_services/cookie-consent.service";
-import { BehaviorSubject, of, Subject } from "rxjs";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { takeUntil } from "rxjs/operators";
-import { SharedModule } from "../../shared/shared.module";
 
 @Component({
   selector: "app-cookie-banner",
-  imports: [CommonModule, FormsModule, SharedModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: "./cookie-banner.component.html",
   styleUrl: "./cookie-banner.component.css",
-
+  standalone: true,
 })
 export class CookieBannerComponent implements OnInit, OnDestroy {
-    @Input() openBanner = new BehaviorSubject<boolean>(false);
+  @Input() openBanner = new BehaviorSubject<boolean>(false);
 
-    showBanner = false;
-    showModal = false;
-    preferences = {
-        marketing: false,
-    };
-    $destroy = new Subject<boolean>();
+  showBanner = false;
+  showModal = false;
+  preferences = {
+    marketing: false,
+  };
 
-    constructor(private consentService: CookieConsentService) {
+  $destroy = new Subject<boolean>();
+
+  constructor(private consentService: CookieConsentService) {}
+
+  ngOnInit(): void {
+    const savedConsent = localStorage.getItem("cookieConsent");
+
+    if (!savedConsent) {
+      this.openBanner.next(true);
+    } else {
+      this.preferences = JSON.parse(savedConsent);
     }
+    this.consentService.applyConsent();
 
-    ngOnInit(): void {
-        const savedConsent = localStorage.getItem("cookieConsent");
-        if (!savedConsent) {
-            this.showBanner = true;
-        } else {
-            this.preferences = JSON.parse(savedConsent);
-        }
-        this.consentService.applyConsent();
+    this.openBanner.pipe(takeUntil(this.$destroy)).subscribe((openBanner) => {
+      this.showBanner = openBanner;
+    });
+  }
 
-        this.openBanner.pipe(takeUntil(this.$destroy)).subscribe(openBanner => {
-            this.showBanner = openBanner;
-        });
-    }
+  ngOnDestroy() {
+    this.openBanner.next(false);
+    this.$destroy.next(null);
+    this.$destroy.complete();
+  }
 
-    ngOnDestroy() {
-        this.openBanner.next(false);
-        this.$destroy.next(null);
-        this.$destroy.complete();
-    }
+  acceptAllCookies(): void {
+    this.preferences = { marketing: true };
+    this.saveAndApplyPreferences();
+  }
 
-    acceptAllCookies(): void {
-        this.preferences = {marketing: true};
-        this.saveAndApplyPreferences();
-    }
+  rejectAllCookies(): void {
+    this.preferences = { marketing: false };
+    this.saveAndApplyPreferences();
+  }
 
-    rejectAllCookies(): void {
-        this.preferences = {marketing: false};
-        this.saveAndApplyPreferences();
-    }
+  savePreferences(): void {
+    this.saveAndApplyPreferences();
+  }
 
-    savePreferences(): void {
-        this.saveAndApplyPreferences();
-    }
+  saveAndApplyPreferences(): void {
+    localStorage.setItem("cookieConsent", JSON.stringify(this.preferences));
+    this.showBanner = false;
+    this.showModal = false;
+    this.openBanner.next(false);
+    this.applyPreferences();
+    window.location.reload();
+  }
 
-    saveAndApplyPreferences(): void {
-        localStorage.setItem("cookieConsent", JSON.stringify(this.preferences));
-        this.showBanner = false;
-        this.showModal = false;
-        this.openBanner.next(false);
-        this.applyPreferences();
-        window.location.reload();
-    }
-
-    applyPreferences(): void {
-        const consent = JSON.parse(localStorage.getItem("cookieConsent") || "{}");
-        this.consentService.applyConsent();
-        // Puoi gestire qui l'inizializzazione di script esterni in base a consent.analytics / marketing
-    }
+  applyPreferences(): void {
+    const consent = JSON.parse(localStorage.getItem("cookieConsent") || "{}");
+  }
 }
