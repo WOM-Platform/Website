202e248033b80129b650ba66abeb3967afe8f1e8
miris-mp <miriam.petrocchi94@gmail.com>
2026-05-11 11:05:11 +0200

    feat(i18n): add translations for home and challenges pages

--- src/Website/src/app/pages/challenge/challenge.component.ts
+++ src/Website/src/app/pages/challenge/challenge.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-challenge',
-  imports: [],
-  templateUrl: './challenge.component.html',
-  styleUrl: './challenge.component.css'
+  selector: "app-challenge",
+  imports: [TranslateModule],
+  templateUrl: "./challenge.component.html",
+  styleUrl: "./challenge.component.css",
 })
-export class ChallengeComponent {
-
-}
+export class ChallengeComponent {}
